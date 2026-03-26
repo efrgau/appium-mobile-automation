5e162196f8a91ea93355f1eaa5ebeddc23bead85
Edwin Pacheco F. <efrgau@gmail.com>
2026-03-25 22:07:12 -0500

    (Fix) Change URL_PATH for APK for tests

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,11 +1,14 @@
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+import os
 
 def create_driver():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    APP_PATH = os.path.join(BASE_DIR, 'apps','ApiDemos-debug.apk')
     options = UiAutomator2Options()
     options.platform_name= 'Android'
     options.device_name= 'Android Emulator'
-    options.app= r"D:\Projects_GIT\Automation\appium-mobile-automation\apps\ApiDemos.apk"
+    options.app= APP_PATH
     options.automation_name='UiAutomator2'
 
     driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
